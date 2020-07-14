00a55cd267a139cec3ea6517e9004223269f96eb
kj-jondell <kj@jondell.com>
2020-07-14 22:40:46 +0200

    clean

--- dtmf/build/libs/CMakeFiles/portaudio.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libportaudio.a"
-  "libportaudio.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/portaudio.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
