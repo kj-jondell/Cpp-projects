0189e3c89c2351eea9234e0bee4cc2506deec550
kj-jondell <kj@jondell.com>
2020-07-28 14:27:43 +0200

    no build

--- Player.h
+++ Player.h
@@ -8,9 +8,6 @@
 #include <iostream>
 #include <string>
 
-#define INPUT_DEVICE_NAME "USB Audio Device: - (hw:2,0)"
-#define OUTPUT_DEVICE_NAME                                                     \
-    "USB Audio Device: - (hw:2,0)" //"Scarlett 8i6 USB: Audio (hw:3,0)"
 #define IN_CHANNELS 1
 #define OUT_CHANNELS 1
 #define FRAMES_PER_BUFFER 512
@@ -20,35 +17,37 @@ using namespace std;
 
 class Player {
 
-  private:
-    int inputDeviceIndex = 0, outputDeviceIndex = 1;
-    PaError err;
-    PaStream *stream;
-    PaStreamParameters outputParameters, inputParameters;
-    Decoder *decoder;
-    Sampler *sampler;
+private:
+  int inputDeviceIndex = 0, outputDeviceIndex = 1;
+  char *inputDeviceName_, *outputDeviceName_;
+  PaError err;
+  PaStream *stream;
+  PaStreamParameters outputParameters, inputParameters;
+  Decoder *decoder;
+  Sampler *sampler;
 
-    bool debug_;
+  bool debug_;
 
-    void initPlayer();
-    int portAudioCallback(const void *inputBuffer, void *outputBuffer,
-                          unsigned long framesPerBuffer,
-                          const PaStreamCallbackTimeInfo *timeInfo,
-                          PaStreamCallbackFlags statusFlags);
-    static int staticPortAudioCallback(const void *inputBuffer,
-                                       void *outputBuffer,
-                                       unsigned long framesPerBuffer,
-                                       const PaStreamCallbackTimeInfo *timeInfo,
-                                       PaStreamCallbackFlags statusFlags,
-                                       void *userData);
+  void initPlayer();
+  int portAudioCallback(const void *inputBuffer, void *outputBuffer,
+                        unsigned long framesPerBuffer,
+                        const PaStreamCallbackTimeInfo *timeInfo,
+                        PaStreamCallbackFlags statusFlags);
+  static int staticPortAudioCallback(const void *inputBuffer,
+                                     void *outputBuffer,
+                                     unsigned long framesPerBuffer,
+                                     const PaStreamCallbackTimeInfo *timeInfo,
+                                     PaStreamCallbackFlags statusFlags,
+                                     void *userData);
 
-  public:
-    Player(bool debug = false);
-    virtual ~Player();
+public:
+  Player(bool debug = false, char *in = "USB Audio Device: - (hw:2,0)",
+         char *out = "USB Audio Device: - (hw:2,0)");
+  virtual ~Player();
 
-    void start() { Pa_StartStream(stream); } // TODO implement error checking
-    void stop() { Pa_CloseStream(stream); }  // TODO implement error checking
-    void openStream();
+  void start() { Pa_StartStream(stream); } // TODO implement error checking
+  void stop() { Pa_CloseStream(stream); }  // TODO implement error checking
+  void openStream();
 };
 
 #endif /* PLAYER_H */
