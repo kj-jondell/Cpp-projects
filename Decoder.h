65c05742ae2b0c289d399388af32803d794cff87
Karl Johannes <kj@jondell.com>
2020-07-28 23:55:05 +0200

    sounds

--- Decoder.h
+++ Decoder.h
@@ -6,10 +6,10 @@
 #include <map>
 #include <vector>
 
-#define DBM_THRESHOLD 0.0005
+#define DBM_THRESHOLD 0.0001
 #define MAX_LOSS 5
 
-#define SEQUENCE_ENTRY_DELAY 20 // given in amt of buffers
+#define SEQUENCE_ENTRY_DELAY 15 // given in amt of buffers
 
 using namespace std;
 
